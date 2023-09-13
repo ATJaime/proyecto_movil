caa6f9fb0f1a4da331760bb8ef751f9061c40400
unknown <ingdanielmendoza@outlook.es>
2023-09-13 12:09:53 -0500

    interfaz test

--- operaciones/lib/ui/test_page.dart
+++ operaciones/lib/ui/test_page.dart
@@ -6,35 +6,60 @@ class TestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MyController controller = Get.find();
-    return const Scaffold(
-        body: Center(
-      child: Column(
-        children: <Widget>[
-          Text("5+3"),
-          Row(
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 100.0), // Ajusta la cantidad de espacio en la parte superior
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FloatingActionButton(onPressed: null, child: Text("1")),
-              FloatingActionButton(onPressed: null, child: Text("4")),
-              FloatingActionButton(onPressed: null, child: Text("7"))
+              Text("5+3"),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton(onPressed: null, child: Text("1")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("4")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("7"))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton(onPressed: null, child: Text("2")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("5")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("8"))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton(onPressed: null, child: Text("3")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("6")),
+                    SizedBox(width: 10.0),
+                    FloatingActionButton(onPressed: null, child: Text("9"))
+                  ],
+                ),
+              ),
+              Center(
+                child: FloatingActionButton(onPressed: null, child: Text("0")),
+              )
             ],
           ),
-          Row(
-            children: <Widget>[
-              FloatingActionButton(onPressed: null, child: Text("2")),
-              FloatingActionButton(onPressed: null, child: Text("5")),
-              FloatingActionButton(onPressed: null, child: Text("8"))
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              FloatingActionButton(onPressed: null, child: Text("3")),
-              FloatingActionButton(onPressed: null, child: Text("6")),
-              FloatingActionButton(onPressed: null, child: Text("9"))
-            ],
-          ),
-          Center(child: FloatingActionButton(onPressed: null, child: Text("0")))
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
